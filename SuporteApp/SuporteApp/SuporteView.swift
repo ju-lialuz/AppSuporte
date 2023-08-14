ea3368f72040ebc0cfc4ac457a157cd52fe61cd9
Júlia Luz <108757926+ju-lialuz@users.noreply.github.com>
2023-08-14 15:06:17 -0300

    Commit teste 2

--- SuporteApp/SuporteApp/SuporteView.swift
+++ SuporteApp/SuporteApp/SuporteView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct SuporteView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Text("Teste da Ju")
+        Text("Teste da Juuuuu")
     }
 }
 
