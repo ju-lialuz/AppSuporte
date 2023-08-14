8c481c791f0a033ceb7f20cfab92171fde004822
Júlia Luz <108757926+ju-lialuz@users.noreply.github.com>
2023-08-14 13:59:59 -0300

    Inicial

---
+++ SuporteApp/SuporteApp/SuporteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuporteAppApp.swift
+//  SuporteApp
+//
+//  Created by Júlia Luz on 14/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SuporteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
