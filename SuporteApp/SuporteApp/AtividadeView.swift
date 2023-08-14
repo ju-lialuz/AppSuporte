015935c30eabe9b5b9a8bd7f400ac858a44ca29a
Júlia Luz <108757926+ju-lialuz@users.noreply.github.com>
2023-08-14 14:55:51 -0300

    Tela de atividades feita

--- SuporteApp/SuporteApp/AtividadeView.swift
+++ SuporteApp/SuporteApp/AtividadeView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct AtividadeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack{
+            VStack(){
+                Image(systemName: "clock.fill")
+                    .font(.system(size: 50))
+                    .foregroundColor(.gray)
+                    .padding(.bottom)
+                
+                Text("Nenhuma atividade de suporte nos últimos 90 dias")
+                    .font(.title2)
+                    .bold()
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom, 3)
+
+
+                
+                Text("Reservas, chamadas e bate-papos passados e futuros com o Suporte da Apple serão exibidos aqui.")
+                    .foregroundColor(.gray)
+                    .multilineTextAlignment(.center)
+                    .frame(width:340)
+
+                    
+            }.navigationTitle("Atividade recente")
+                .padding(.horizontal, 50)
+        }
     }
 }
 
